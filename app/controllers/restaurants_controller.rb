41bc1fb7f2fa2eb2cd710e34f6e9f3a5c5bcd81b
Vinicius Stelet <vinicius.bootcamp@gmail.com>
2020-11-27 16:51:24 -0300

    View new.html

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -12,10 +12,20 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.new
   end
 
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+    else
+      render :new
+    end
+  end
+
   private
 
   # Only allow a trusted parameter "white list" through.
   def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :rating)
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
