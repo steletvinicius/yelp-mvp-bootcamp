25f8839bce20efaff573460fc5e71251c89f16fd
Vinicius Stelet <vinicius.bootcamp@gmail.com>
2020-11-27 13:43:17 -0300

    Validations for Restaurant and review models implemented

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,6 @@
 class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :content, presence: true
+  validates :rating, presence: true, numericality: true, inclusion: [0, 1, 2, 3, 4, 5]
 end
