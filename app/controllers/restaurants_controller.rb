ade0487bab03583aee1d54b7ac5ebd5acbbe1816
Vinicius Stelet <vinicius.bootcamp@gmail.com>
2020-11-27 16:20:23 -0300

    View index.html working

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,30 @@
 class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  # def show
+  #   @restaurant = Restaurant.find(params[:id])
+  # end
+
+  # def new
+  #   @restaurant = Restaurant.new
+  # end
+
+  # def create
+  #   @restaurant = Restaurant.new(restaurant_params)
+  #   if @restaurant.save
+  #     redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+  #   else
+  #     render :new
+  #   end
+  # end
+
+  # private
+
+  # # Only allow a trusted parameter "white list" through.
+  # def restaurant_params
+  #   params.require(:restaurant).permit(:name, :address, :rating)
+  # end
 end
